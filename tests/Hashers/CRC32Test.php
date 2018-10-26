666f2907a140ba838157890b1430ff074d7a4235
Michael Cordingley <Michael.Cordingley@gmail.com>
2018-10-26 19:36:54 -0400

    Add test for CRC32.

--- tests/Hashers/CRC32Test.php
+++ tests/Hashers/CRC32Test.php
@@ -2,15 +2,14 @@
 
 namespace mcordingley\HashBinTests\Hashers;
 
-use mcordingley\HashBin\Hashers\IntegerInterpreters\Unpack;
-use mcordingley\HashBin\Hashers\Native;
+use mcordingley\HashBin\Hashers\CRC32;
 use PHPUnit\Framework\TestCase;
 
 final class Sha256Test extends TestCase
 {
     public function testHash()
     {
-        $hasher = new Native('sha256', new Unpack);
+        $hasher = new CRC32;
 
         static::assertTrue(is_int($hasher->hash('test')));
     }
